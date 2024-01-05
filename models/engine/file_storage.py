490f8f70cd7179260c1bbf005f04e9bdc32a3367
Haruna Bah <harunjibs@gmail.com>
2024-01-05 16:22:27 +0000

    file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -47,18 +47,13 @@ class FileStorage:
                 return res[0]
         return None
 
-    def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
-        if obj def get(self, cls, id):
-        """retrieves an object of a class with id"""
-        obj = None
-        if cls is not None and issubclass(cls, BaseModel):
-            obj = self.__session.query(cls).filter(cls.id == id).first()
-        return obj
-
     def count(self, cls=None):
         """retrieves the number of objects of a class or all (if cls==None)"""
-        return len(self.all(cls)) not None:
+        return len(self.all(cls))
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        if obj is not None:
             key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
 
@@ -77,7 +72,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
